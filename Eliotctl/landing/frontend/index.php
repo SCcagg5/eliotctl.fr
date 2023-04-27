f8f156f1abfe914dac0c11c3d1b072542677324a
SCcagg5 <sccagg5@gmail.com>
2023-04-27 14:48:36 +0200

    Update index.php

--- Eliotctl/landing/frontend/index.php
+++ Eliotctl/landing/frontend/index.php
@@ -30,7 +30,10 @@
         <div class="l-wrapper">
           <header class="header">
             <a class="header--logo" href="/">
-            <p>Eliotctl.fr</p><img src="assets/img/logo.webp" style="width: 69px;position: absolute;z-index: -1;left: 16px;" alt="Welcome"></a> <button class="header--cta cta">Contact me</button>
+            <p>Eliotctl.fr</p>
+              <img src="assets/img/logo.webp" style="width: 69px;position: absolute;z-index: -1;left: 16px;" alt="logo" />
+            </a> 
+            <button class="header--cta cta">Contact me</button>
             <div class="header--nav-toggle"></div>
           </header>
           <nav class="l-side-nav">
@@ -45,12 +48,21 @@
             <li class="l-section section" id="home">
               <div class="intro">
                 <div class="intro--banner">
-                <h1>Innovative<br>
-                devops<br>
-                work</h1><button class="cta">Contact <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewbox="0 0 150 118" style="enable-background:new 0 0 150 118;" xml:space="preserve">
-                <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
-                  <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z"></path>
-                </g></svg></button> <img src="assets/img/test1.webp" alt="Welcome"></div>
+                  <h1>
+                    Innovative<br>
+                    devops<br>
+                    work
+                  </h1>
+                  <button class="cta">
+                    Contact 
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewbox="0 0 150 118" style="enable-background:new 0 0 150 118;" xml:space="preserve">
+                      <g transform="translate(0.000000,118.000000) scale(0.100000,-0.100000)">
+                        <path d="M870,1167c-34-17-55-57-46-90c3-15,81-100,194-211l187-185l-565-1c-431,0-571-3-590-13c-55-28-64-94-18-137c21-20,33-20,597-20h575l-192-193C800,103,794,94,849,39c20-20,39-29,61-29c28,0,63,30,298,262c147,144,272,271,279,282c30,51,23,60-219,304C947,1180,926,1196,870,1167z"></path>
+                      </g>
+                    </svg>
+                  </button> 
+                  <img src="assets/img/test1.webp" alt="Welcome" />
+                </div>
                 <div class="intro--options">
                   <a href="#projects" onclick="scrollTozone('#projects')">
                   <h3>WebApps</h3>
